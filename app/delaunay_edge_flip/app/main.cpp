0325ceea61b160b1ed96ac8041b6169bb6c12f14
Serban Porumbescu <sdporumbescu@ucdavis.edu>
2023-01-04 21:17:38 -0800

    Add basic mesh input and output options

--- app/delaunay_edge_flip/app/main.cpp
+++ app/delaunay_edge_flip/app/main.cpp
@@ -9,10 +9,10 @@
 #include <igl/writeDMAT.h>
 
 
-#include <stdlib.h>
 #include <chrono>
 #include <cstdlib>
 #include <iostream>
+#include <cassert>
 
 using namespace wmtk;
 using namespace app::def;
@@ -25,4 +25,27 @@ extern "C" {
 int main(int argc, char** argv)
 {
     std::cout << "Hello from def\n";
+
+    std::string input_mesh_name;
+    std::string output_mesh_name = "out.obj";
+    int thread = 1;
+
+    CLI::App app{argv[0]};
+
+    auto input_option = app.add_option("-i, --input", input_mesh_name, "Input mesh.")->check(CLI::ExistingFile);
+    input_option->required();
+
+    auto output_option = app.add_option("-o, --output", output_mesh_name, "Output mesh.");
+    app.add_option("-j, --thread", thread, "thread.");
+
+    CLI11_PARSE(app, argc, argv);
+
+    Eigen::MatrixXd V;
+    Eigen::MatrixXi F;
+    bool ok = igl::read_triangle_mesh(input_mesh_name, V, F);
+    assert(ok != false);
+
+    ok = igl::write_triangle_mesh(output_mesh_name, V, F);
+    assert(ok != false);
+
 }
